3ea6cfacc8148f39c5cf3effa277c0c663cfa720
equis3351 <equis3351@naver.com>
2024-06-18 17:43:00 +0900

    init

---
+++ src/main/java/com/sparta/javajyojo/JavajyoJoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.javajyojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajyoJoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavajyoJoApplication.class, args);
+    }
+
+}
