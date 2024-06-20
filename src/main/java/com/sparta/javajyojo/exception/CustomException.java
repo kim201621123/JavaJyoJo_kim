c37cc1c2737f36685027928e08aa96278b71dda1
equis3351 <130730656+equis3351@users.noreply.github.com>
2024-06-20 12:05:53 +0900

    🎉 add : Exception Handler

---
+++ src/main/java/com/sparta/javajyojo/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.sparta.javajyojo.exception;
+
+import com.sparta.javajyojo.enums.ErrorType;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private String result;
+    private ErrorType errorType;
+
+    public CustomException(ErrorType errorType) {
+        this.result = "ERROR";
+        this.errorType = errorType;
+    }
+}
