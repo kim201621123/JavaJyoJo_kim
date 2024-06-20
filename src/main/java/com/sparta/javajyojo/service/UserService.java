04e307dde2abeb931597e7bb26e7a931020e249e
equis3351 <130730656+equis3351@users.noreply.github.com>
2024-06-20 12:06:18 +0900

    ♻️ refactor : UserService Exception

--- src/main/java/com/sparta/javajyojo/service/UserService.java
+++ src/main/java/com/sparta/javajyojo/service/UserService.java
@@ -4,6 +4,8 @@ import com.sparta.javajyojo.dto.ProfileRequestDto;
 import com.sparta.javajyojo.dto.ProfileResponseDto;
 import com.sparta.javajyojo.dto.SignUpRequestDto;
 import com.sparta.javajyojo.entity.User;
+import com.sparta.javajyojo.enums.ErrorType;
+import com.sparta.javajyojo.exception.CustomException;
 import com.sparta.javajyojo.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +30,7 @@ public class UserService {
         Optional<User> optionalUser = userRepository.findByUsername(username);
 
         if (optionalUser.isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
+            throw new CustomException(ErrorType.DUPLICATE_ACCOUNT_ID);
         }
 
         User user = new User(
@@ -59,33 +61,33 @@ public class UserService {
         if (requestDto.getPassword() != null) {
 //            // 본인 확인을 위해 현재 비밀번호를 입력하여 올바른 경우
 //            if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
-//                throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
+//                throw new CustomException(ErrorType.INVALID_PASSWORD);
 //            }
 //            //현재 비밀번호와 동일한 비밀번호로는 변경할 수 없음
 //            if (passwordEncoder.matches(requestDto.getPassword(), requestDto.getNewPassword())) {
-//                throw new IllegalArgumentException("현재 비밀번호와 동일한 비밀번호로 변경할 수 없습니다.");
+//                throw new CustomException(ErrorType.PASSWORD_SAME);
 //            }
 //            // 최근 3번안에 사용한 비밀번호는 사용할 수 없도록 제한
 //            boolean isInPreviousPasswords = user.getPwUsdLst3Tms().stream()
 //                    .anyMatch(pw -> passwordEncoder.match(requestDto.getNewPassword(), pw));
 //            if (isInPreviousPasswords) {
-//                throw new IllegalArgumentException("이전 비밀번호와 동일한 비밀번호로 변경할 수 없습니다.");
+//                throw new CustomException(ErrorType.PASSWORD_RECENTLY_USED);
 //            }
 //            newEncodePassword = passwordEncoder.encode(requestDto.getNewPassword());
 
             // 본인 확인을 위해 현재 비밀번호를 입력하여 올바른 경우
             if (!requestDto.getPassword().equals(user.getPassword())) {
-                throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
+                throw new CustomException(ErrorType.INVALID_PASSWORD);
             }
             // 현재 비밀번호와 동일한 비밀번호로는 변경할 수 없음
             if (requestDto.getPassword().equals(requestDto.getNewPassword())) {
-                throw new IllegalArgumentException("현재 비밀번호와 동일한 비밀번호로 변경할 수 없습니다.");
+                throw new CustomException(ErrorType.PASSWORD_SAME);
             }
             // 최근 3번안에 사용한 비밀번호는 사용할 수 없도록 제한
             boolean isInPreviousPasswords = user.getPwUsdLst3Tms().stream()
                     .anyMatch(pw -> pw.equals(requestDto.getNewPassword()));
             if (isInPreviousPasswords) {
-                throw new IllegalArgumentException("이전 비밀번호와 동일한 비밀번호로 변경할 수 없습니다.");
+                throw new CustomException(ErrorType.PASSWORD_RECENTLY_USED);
             }
 
             newPassword = requestDto.getNewPassword();
@@ -103,7 +105,7 @@ public class UserService {
 
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("해당 사용자를 찾을 수 없습니다.")
+                () -> new CustomException(ErrorType.NOT_FOUND_USER)
         );
     }
 
