aed67a6fc99e563c0f8c85fca3f739db34d70a49
equis3351 <130730656+equis3351@users.noreply.github.com>
2024-06-22 11:40:46 +0900

    ♻️ refactor : Repository

--- src/main/java/com/sparta/javajyojo/repository/MenuRepository.java
+++ src/main/java/com/sparta/javajyojo/repository/MenuRepository.java
@@ -4,5 +4,4 @@ import com.sparta.javajyojo.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-    Menu findByMenuId(Long menuId);
 }
